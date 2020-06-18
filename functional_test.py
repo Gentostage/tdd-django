4bba00543a1d97f343d3aaae6d33789305a09a8d
Minkin Stepan <getnostage@gmail.com>
2020-06-18 11:11:54 +0900

    add unittest

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,23 @@
 from selenium import webdriver
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+import unittest
+
+class NewVisionTest(unittest.TestCase):
+
+    def setUp(self):
+        '''установка'''
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        '''Демонтаж'''
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_laster(self):
+        '''Начало теста'''
+
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Тест Закончен')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
