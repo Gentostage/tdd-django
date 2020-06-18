2ec852b1497ac2b94e81eb22cbe8f74445554d1b
Minkin Stepan <getnostage@gmail.com>
2020-06-18 17:24:26 +0900

    refactoring home page for uses templates

--- lists/tests.py
+++ lists/tests.py
@@ -9,16 +9,7 @@ from lists.views import home_page
 class SomeTest(TestCase):
     '''Тест на токсичность'''
     
-    def test_root_url_resolves_to_home_page(self):
-        '''Тест корневого url'''
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
-    def test_home_page_return_correct_html(self):
-        '''проверка на првильный html домашней страницы'''
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+    def test_uses_home_template(self):
+        '''Тест шаблона домашней страницы'''
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
