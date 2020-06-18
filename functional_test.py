7c05fe18fa496c1d35bc3790817dfefc28525797
Minkin Stepan <getnostage@gmail.com>
2020-06-18 16:55:00 +0900

    FT possible to enter el list

--- functional_test.py
+++ functional_test.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVisionTest(unittest.TestCase):
 
@@ -16,6 +18,24 @@ class NewVisionTest(unittest.TestCase):
 
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
+        headr_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', headr_text)
+
+        inputbox = self.browser.find_elements_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        inputbox.send_keys('Купить павлиньи перья')
+        time.sleep(1)
+
+        table = self.browser.find_elements_by_id('id_list_table')
+        rows = table.find_elements_by_id('tr')
+        self.assertTrue(
+            any(row.text == '1: Купить павлиньи перья' for row in rows)
+        )
+        
         self.fail('Тест Закончен')
 
 
