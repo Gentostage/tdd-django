aa0cc8ece2b84edbdc0c699b4c2ccf0912e82905
Minkin Stepan <getnostage@gmail.com>
2020-06-19 12:19:22 +0900

    FT add func check el in teble

--- functional_test.py
+++ functional_test.py
@@ -13,6 +13,12 @@ class NewVisionTest(unittest.TestCase):
         '''Демонтаж'''
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        ''' проврка строки в таблице списка'''
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_laster(self):
         '''Начало теста'''
 
@@ -38,11 +44,8 @@ class NewVisionTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
-        self.assertIn(
-            '2: Сделать мушку из павлиньих перьев',
-            [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Купить павлиньи перья')
+        self.check_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
         
 
         self.fail('Тест Закончен')
