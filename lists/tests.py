95793e03e071c9dacc618f2f6bcfc93862ec7128
Minkin Stepan <getnostage@gmail.com>
2020-06-19 16:29:13 +0900

    redirect after post  and views all el in template

--- lists/tests.py
+++ lists/tests.py
@@ -17,9 +17,31 @@ class SomeTest(TestCase):
 
     def test_can_save_a_POST_request(self):
         '''Тест сохранение пост запроса'''
-        response = self.client.post('/', data={'input_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+        self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+        
+    def test_redirect_after_post(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+    
+    def test_only_saved_item_when_necessary(self):
+        '''Сохраняет элементы только когда нужно'''
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_display_all_list_items(self):
+        '''тест: отображения всех елементов'''
+        Item.objects.create(text='item 1')
+        Item.objects.create(text='item 2')
+
+        response = self.client.get('/')
+        
+        self.assertIn('item 1', response.content.decode())
+        self.assertIn('item 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
