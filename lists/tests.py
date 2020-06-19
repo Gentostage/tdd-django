4a74b8d391f867f6019d271c1e01e2934581bf8c
Minkin Stepan <getnostage@gmail.com>
2020-06-19 12:45:38 +0900

    Model Item and migrations

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from django.urls import resolve
 from django.http import HttpRequest
 
 from lists.views import home_page
+from lists.models import Item
 
 
 # Create your tests here.
@@ -16,6 +17,28 @@ class SomeTest(TestCase):
 
     def test_can_save_a_POST_request(self):
         '''Тест сохранение пост запроса'''
-        response = self.client.post('/', data={'item_text': 'A new list item'})
+        response = self.client.post('/', data={'input_text': 'A new list item'})
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+
+class ItemModelTest(TestCase):
+    '''Тест модели списка'''
+
+    def test_save_and_retrieving_items(self):
+        '''Тест сохраннение и получение элементов списка'''
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_item = Item.objects.all()
+        self.assertEqual(saved_item.count(), 2)
+
+        first_saved_item = saved_item[0]
+        secomd_saved_item = saved_item[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(secomd_saved_item.text, 'Item the second')
