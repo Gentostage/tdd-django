ed39634a5513221e097dce2c54de5e3e4a9853b7
Minkin Stepan <getnostage@gmail.com>
2020-06-22 16:22:43 +0900

    FT add multi users and uniq url

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,14 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import unittest
 import time
 
+MAX_WAIT = 10
+
 class NewVisionTest(LiveServerTestCase):
+
 
     def setUp(self):
         '''установка'''
@@ -14,14 +18,23 @@ class NewVisionTest(LiveServerTestCase):
         '''Демонтаж'''
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        ''' проврка строки в таблице списка'''
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_now_in_list_table(self, row_text):
+        '''Ожидание строки в таблице'''
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
-    def test_can_start_a_list_and_retrieve_it_laster(self):
-        '''Начало теста'''
+
+    def test_can_start_a_list_for_one_user(self):
+        '''Тест начать список для одного пользователя'''
 
         self.browser.get(self.live_server_url)
         self.assertIn('To-Do', self.browser.title)
@@ -36,20 +49,51 @@ class NewVisionTest(LiveServerTestCase):
 
         inputbox.send_keys('Купить павлиньи перья')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        self.wait_for_now_in_list_table('1: Купить павлиньи перья')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Сделать мушку из павлиньих перьев')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.check_for_row_in_list_table('1: Купить павлиньи перья')
-        self.check_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
+        self.wait_for_now_in_list_table('1: Купить павлиньи перья')
+        self.wait_for_now_in_list_table('2: Сделать мушку из павлиньих перьев')
         
-
         self.fail('Тест Закончен')
+
+
+    def test_multiple_users_can_start_lists_at_different(self):
+        '''Тест Разные пользователи разные url списки'''
+
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Купить павлиньи перья')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_now_in_list_table('1: Купить павлиньи перья')
+
+        edit_list_url = self.browser.current_url
+        self.assertRegex(edit_list_url, '/lists/.+')
+
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Купить павлиньи перья', page_text)
+        self.assertNotIn('Сделать мушку', page_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Купить молоко')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Купить молоко')
+
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Купить павлиньи перья', page_text)
+        self.assertIn('Купить молоко', page_text)
+
 
 
 if __name__ == '__main__':
